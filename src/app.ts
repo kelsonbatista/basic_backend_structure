26265643bc191f9dcc7292cdc6b863a63fbdb72d
kelsonbatista <kelsonbatista@gmail.com>
2022-11-26 00:49:03 -0300

    refactor(app): added header access methods

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,10 @@ class App {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "*");
-      res.header("Access-Control-Allow-Methods", "*");
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+      );
       next();
     };
     this.app.use(accessControl);
